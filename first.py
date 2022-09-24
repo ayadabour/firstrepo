6c918faeb2d9f3c1c292ca8adf17b76af4a5e565
ayadabour <114313516+ayadabour@users.noreply.github.com>
2022-09-24 22:49:44 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,19 @@
+"""cm = int(input('Enter your height in cm: '))
+
+inches = cm / 2.54
+ft = inches // 12
+inches = inches % 12
+
+print('Your height is',int(ft),'feet and',inches,'inches.')
+x=int(input("enter number"))
+if (x%2==0):
+   print(x ,"is even number")
+else:
+   print(x ,"is odd number")"""
+for x in range(10):
+    if (x%2==0):
+        print(x ,"is even number")
+    else:
+        print(x ,"is odd number")
+
+
